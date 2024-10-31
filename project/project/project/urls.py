ecbaf0a0db68966629aa906a3320d76311becd18
Bryium <bryiumonyancha@gmail.com>
2024-11-01 00:14:42 +0300

    defined the end point of api calls

--- project/project/project/urls.py
+++ project/project/project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from project.app.views import ReactView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", ReactView.as_View, name="anything"),
 ]
